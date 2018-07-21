8a0fdc0cf8b88d4eaf58284344fcfbf23ffd4d2b
rawando513 <rawan_meid@hotmail.com>
2018-07-21 16:18:17 +0300

    add user sign in/up

--- app/src/main/java/remoty/internship/wadimakkah/remotyapplication/MainActivity.java
+++ app/src/main/java/remoty/internship/wadimakkah/remotyapplication/MainActivity.java
@@ -5,13 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView alreadyAcount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<< HEAD
+
         setContentView(R.layout.activity_main);
 
         Button signUpDesignerButton = (Button)findViewById(R.id.SignUpDesignerBtn1);
@@ -22,8 +24,26 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(toDesignerSignUp);
             }
         });
-=======
+        Button signUpUser = (Button)findViewById(R.id.signUpUserBtn2);
+        signUpUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toDesignerSignUp = new Intent(MainActivity.this,UserSignUpActivity.class);
+                startActivity(toDesignerSignUp);
+            }
+        });
+        alreadyAcount = (TextView) findViewById(R.id.tosignPage);
+
+        alreadyAcount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent toSinInPage = new Intent(MainActivity.this,DesignerSignInActivity.class);
+                startActivity(toSinInPage);
+            }
+        });
+
+
         setContentView(R.layout.activity_user_sign_up);
->>>>>>> 38d606b90ef764e19cd4c044fecb4dfcad5f7d39
+
     }
 }
